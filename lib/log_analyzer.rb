1eb6bcf30fe86d0272cdfbcbfa481ba37cd6e2cd
ali hong <ali.hong@getbraintree.com>
2020-09-20 13:57:51 +0000

    sort requires

--- lib/log_analyzer.rb
+++ lib/log_analyzer.rb
@@ -1,12 +1,12 @@
+require "csv"
 require "fileutils"
 require "logger"
-require "csv"
 
 require "log_analyzer/analyzer"
 require "log_analyzer/bucket"
 require "log_analyzer/log_clock"
-require "log_analyzer/rolling_window_traffic_check"
 require "log_analyzer/log_parser"
+require "log_analyzer/rolling_window_traffic_check"
 require "log_analyzer/timeseries_data_point"
 require "log_analyzer/version"
 
